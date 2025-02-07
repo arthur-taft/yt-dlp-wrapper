a4de5147e9d32b0f26f8c1ba60f57485eec5ce15
Arthur Taft <arthur.taft2006@icloud.com>
2025-02-07 01:27:26 -0700

    Remove Hard Coded Values
    Removed hard coded values from the script

--- yt-dlp-wrapper.ps1
+++ yt-dlp-wrapper.ps1
@@ -1,17 +1,28 @@
 ﻿# Set Variables
-$Path = Join-Path -Path $HOME -ChildPath "\Documents\YouTube"
-$exe = Join-Path -Path $Path -ChildPath "\yt-dlp.exe"
+$current_dir = Get-Location
+$exe_location = Join-Path -Path $current_dir -ChildPath '\bin\yt-dlp.exe'
+$audio_dir = Join-Path -Path $current_dir -ChildPath '\audio'
+$video_dir = Join-Path -Path $current_dir -ChildPath '\video'
+$test_audio_dir = Test-Path -Path $audio_dir
+$test_video_dir = Test-Path -Path $video_dir
 $val = 0
 $url = ""
 
-# Switch to directory with executable
-Set-Location $Path
-
 # Remove old executable
-Remove-Item yt-dlp.exe
+Remove-Item $exe_location
 
 # Download new executable
-Invoke-WebRequest https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe -OutFile $exe
+Invoke-WebRequest https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe -OutFile $exe_location
+
+# Check if audio output directory exists, if not, create it
+if($test_audio_dir -eq $false) {
+    New-Item -Path $current_dir -Name "audio" -ItemType "directory"
+}
+
+# Check if video output directory exists, if not, create it
+if($test_video_dir -eq $false) {
+    New-Item -Path $current_dir -Name "video" -ItemType "directory"
+}
 
 
 while($val -ne 1) {
@@ -21,13 +32,13 @@ while($val -ne 1) {
     # Download audio
     if($Type -eq "audio") {
         $url = Read-Host "Enter URL Here"
-        ./yt-dlp.exe -x -f 'ba' --audio-format mp3 -o $HOME'\Music\YouTube\output\%(title)s.%(ext)s' $url
+        Invoke-Expression "$exe_location -x -f 'ba' --audio-format mp3 -o $audio_dir'\%(title)s.%(ext)s' $url"
         $val++
     }
     # Download video
     elseif($Type -eq "video"){
         $url = Read-Host "Enter URL Here"
-        ./yt-dlp.exe -f 'bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b' -o $HOME'\Video\output\%(title)s.%(ext)s' $url
+        Invoke-Expression "$exe_location -f 'bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b' -o $video_dir'\%(title)s.%(ext)s' $url"
         $val++
     }
     else {
