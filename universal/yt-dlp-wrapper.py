721b1e44c6c4185d3d009279404cd1baedaea7b1
Arthur Taft <arthur.taft2006@icloud.com>
2026-06-07 17:16:09 -0600

    update stuff and things

--- universal/yt-dlp-wrapper.py
+++ universal/yt-dlp-wrapper.py
@@ -9,43 +9,92 @@ project_home = dirs.get_home()
 audio_dir = dirs.get_audio_dir()
 video_dir = dirs.get_video_dir()
 
-audio_title = Path.joinpath(audio_dir, '%(title)s.%(ext)s')
-video_title = Path.joinpath(video_dir, '%(title)s.%(ext)s')
+audio_title = Path.joinpath(audio_dir, "%(title)s.%(ext)s")
+video_title = Path.joinpath(video_dir, "%(title)s.%(ext)s")
+split_video_title = Path.joinpath(video_dir, "chapter:%(section_title)s.%(ext)s")
 
-batch_dl_file = Path.joinpath(project_home, 'batch_dl.txt')
-requirements = Path.joinpath(project_home, 'requirements.txt')
+batch_dl_file = Path.joinpath(project_home, "batch_dl.txt")
+requirements = Path.joinpath(project_home, "requirements.txt")
 
-parser = argparse.ArgumentParser(description='A wrapper script to yt-dlp')
+parser = argparse.ArgumentParser(description="A wrapper script to yt-dlp")
 
-parser.add_argument('--batch', '-b', type=bool, help='Perform batch download')
+parser.add_argument("--batch", "-b", type=bool, help="Perform batch download")
 
 args = parser.parse_args()
 
+
 def batch_convert():
-    format = str(input('Audio or Video?: ')).lower()
+    format = str(input("Audio or Video?: ")).lower()
 
     match format:
         case "audio":
-            run(['yt-dlp', '-x', '-f', 'ba', '--audio-format', 'mp3', '-o', f'{audio_title}', '-a', f'{batch_dl_file}'])
+            run(
+                [
+                    "yt-dlp",
+                    "-x",
+                    "-f",
+                    "ba",
+                    "--audio-format",
+                    "mp3",
+                    "-o",
+                    f"{audio_title}",
+                    "-a",
+                    f"{batch_dl_file}",
+                ]
+            )
         case "video":
-            run(['yt-dlp', '-f', 'bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b', '--sponsorblock-remove', 'sponsor,selfpromo,interaction', '-o', f'{video_title}', '-a', f'{batch_dl_file}'])
+            run(
+                [
+                    "yt-dlp",
+                    "-f",
+                    "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b",
+                    "--sponsorblock-remove",
+                    "sponsor,selfpromo,interaction",
+                    "-o",
+                    f"{video_title}",
+                    "-a",
+                    f"{batch_dl_file}",
+                ]
+            )
         case _:
             print("Response must be 'Audio' or 'Video'!")
 
+
 def normal_convert():
     downloads_complete = False
-    while downloads_complete != True: # type: ignore
-        format = str(input('Audio or Video?: ')).lower()
+    while not downloads_complete:  # type: ignore
+        format = str(input("Audio or Video?: ")).lower()
 
-        url = str(input('Enter URL here: '))
+        url = str(input("Enter URL here: "))
 
         val = False
 
         match format:
             case "audio":
-                run(['yt-dlp', '-x', '-f', 'ba', '--audio-format', 'wav', '-o', f'{audio_title}', f'{url}'])   
-                while val != True:
-                    do_continue = str(input("Do you want to convert another file? (y/n): ")).lower()
+                run(
+                    [
+                        "yt-dlp",
+                        "-x",
+                        "-f",
+                        "ba",
+                        "--audio-format",
+                        "wav",
+                        "--cookies-from-browser",
+                        "firefox",
+                        "--sponsorblock-remove",
+                        "sponsor,selfpromo,interaction",
+                        "--split-chapters",
+                        "-P",
+                        "audio",
+                        "-o",
+                        f"{audio_title}",
+                        f"{url}",
+                    ]
+                )
+                while not val:
+                    do_continue = str(
+                        input("Do you want to convert another file? (y/n): ")
+                    ).lower()
                     match do_continue:
                         case "y":
                             val = True
@@ -55,9 +104,33 @@ def normal_convert():
                         case _:
                             print("Answer must be Y or N!")
             case "video":
-                run(['yt-dlp', '-f', 'bv*[ext=mkv]+ba[ext=m4a]/b[ext=mkv] / bv*+ba/b', '--sponsorblock-remove', 'sponsor,selfpromo,interaction', '-o', f'{video_title}', f'{url}'])
-                while val != True:
-                    do_continue = str(input("Do you want to convert another file? (y/n): ")).lower()
+                run(
+                    [
+                        "yt-dlp",
+                        "-f",
+                        "bv*[ext=mkv]+ba[ext=m4a]/b[ext=mkv] / bv*+ba/b",
+                        "--merge-output-format",
+                        "mkv",
+                        "--sponsorblock-remove",
+                        "sponsor,selfpromo,interaction",
+                        "--split-chapters",
+                        # "--write-subs",
+                        # "--sub-format",
+                        # "srt",
+                        # "--sub-langs",
+                        # "en.*,-live-chat",
+                        # "--embed-subs",
+                        "--cookies-from-browser",
+                        "firefox",
+                        "-o",
+                        f"{video_title}",
+                        f"{url}",
+                    ]
+                )
+                while not val:
+                    do_continue = str(
+                        input("Do you want to convert another file? (y/n): ")
+                    ).lower()
                     match do_continue:
                         case "y":
                             val = True
@@ -69,7 +142,8 @@ def normal_convert():
             case _:
                 print("Response must be 'Audio' or 'Video'!")
 
-run(['pip', 'install', '--upgrade', '-r', f'{requirements}'])
+
+run(["pip", "install", "--upgrade", "-r", f"{requirements}"])
 
 if not audio_dir.exists():
     audio_dir.mkdir()
@@ -77,7 +151,7 @@ if not audio_dir.exists():
 if not video_dir.exists():
     video_dir.mkdir()
 
-if args.batch == True:
+if args.batch:
     batch_convert()
 else:
     normal_convert()
